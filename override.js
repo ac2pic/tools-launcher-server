4813526604379ef97ee21d7736b2d4869c0e9686
ac2pic <ac2pic@gmail.com>
2020-05-11 13:24:34 -0500

    Added url and assets redirect code

---
+++ override.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+
+
+
+exports.Redirect = class Redirect {
+
+    constructor(new_path) {
+        this.redirect = new_path;
+    }
+
+    sendResponse(res) {
+        res.redirect(this.redirect);
+    }
+};
+
+class AssetData {
+    constructor(settings) {
+        this.headers = settings.headers;
+        this.data = settings.data;
+    }
+
+    sendResponse(res) {
+        res.set(this.headers);
+        res.status(200).send(this.data);
+    }
+}
+
+
+
+exports.AssetsOverride = class AssetsOverride {
+    /**
+     * 
+     * @param {string} url 
+     * @param {} data 
+     */
+    constructor(data) {
+        this.data = new AssetData(data);
+    }
+
+    sendResponse(res) {
+        this.data.sendResponse(res);
+    }
+};
