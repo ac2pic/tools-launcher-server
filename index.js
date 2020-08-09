112da1b764749b40d3ab4c9432e4de76747e83ab
ac2pic <ac2pic@gmail.com>
2020-08-09 11:19:19 -0500

    Added more api stuff + moved config to external file

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 
 const port = 4000;
 
-const pathToCrossCode = String.raw`C:\Program Files (x86)\Steam\steamapps\common\CrossCode`;
+const {pathToCrossCode} = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const { Redirect, AssetsOverride } = require('./override.js');
 
 
 app.use(function (req, res, next) {
@@ -22,9 +23,81 @@ app.use(function (req, res, next) {
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(bodyParser.json());
 
 app.use(express.static(pathToCrossCode));
+
+const resourceOverride = new Map;
+
+app.post('/api/redirect/clear', (req, res) => {
+    resourceOverride.clear();
+    res.status(200).send('Success');
+});
+
+app.get('/api/get-tools', (req, res) => {
+    // check path 
+    const files = fs.readdirSync(path.join(pathToCrossCode, 'tools'));
+    const tools = [];
+    for (const file of files) {
+        try {
+            const relativePath = path.posix.join('tools', file + '/');
+            const toolConfigPath = path.posix.join(pathToCrossCode, relativePath, 'tool.config.json');
+            const toolConfig = JSON.parse(fs.readFileSync(toolConfigPath, 'utf8'));
+            toolConfig.path = '/' + relativePath;
+            tools.push(toolConfig);
+        } catch (e) {}
+    }
+    res.status(200).json(tools);
+
+});
+
+const ccmods = {};
+
+app.get('*', async (req, res, next) => {
+    if (req.url.includes('.ccmod/')) {
+        // check if mod found
+        // if not found, logic to unzip
+        // logic to 
+    }
+    next();
+});
+
+app.post('/api/redirect/set', (req, res) => {
+    // format: [{"url": string, "redirect": string}]
+    if (!Array.isArray(req.body)) {
+        res.status(500).send('Body must be an array ');
+        return;
+    }
+
+    let failed = false;
+    const tempResourceMapping = new Map;
+    for (const { url, redirect } of req.body) {
+        if (!url || !redirect) {
+            failed = true;
+            break;
+        }
+        tempResourceMapping.set(url, new Redirect(redirect));
+    }
+
+    if (failed) {
+        res.status(500).send('Body array objects must be in format {"url": "", "redirect": ""}');
+    } else {
+        for (const [key, value] of tempResourceMapping) {
+            resourceOverride.set(key, value);
+        }
+        res.status(200).send('Success');
+    }
+});
+
+app.get('*', (req, res, next) => {
+    if (resourceOverride.has(req.path)) {
+        resourceOverride.get(req.path).sendResponse(res);
+    } else {
+        next();
+    }
+});
+
 
 // EXPERIMENTAL
 app.post('/api/make-dir/*', (req, res, next) => {
@@ -36,7 +109,6 @@ app.post('/api/make-dir/*', (req, res, next) => {
     res.sendStatus(200);
 });
 
-
 app.post('/api/save/*', (req, res, next) => {
     const savePath = req.path.split("/").slice(3).join('/');
     if (savePath.length) {
@@ -46,5 +118,7 @@ app.post('/api/save/*', (req, res, next) => {
     res.sendStatus(200);
 });
 
+
+app.use(express.static(pathToCrossCode));
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
